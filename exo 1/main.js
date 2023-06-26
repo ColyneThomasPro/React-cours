370982e44989606ff64becab6c3c2e5380087446
Colyne Thomas <colyne.thomas300403@gmail.com>
2023-06-26 14:47:30 +0200

    [+]création d'une fonction générique pour créer un élément en html

--- exo 1/main.js
+++ exo 1/main.js
@@ -1,6 +1,19 @@
 // cibler le bouton en html
 const button = document.querySelector("button");
 
+ // je créer une fonction générique pour créer un élément "createElementNode" à laquelle j'attribut différe,ts paramètres : ici son type, et en optionnel son nom de classe, du texte et une source d'image
+const  createElementNode = (tagType, className = "", text = "", srcImg = "") => {
+    const nodeElement = document.createElement(tagType)
+    nodeElement.setAttribute("class", className)
+    nodeElement.setAttribute("src", srcImg)
+
+    nodeElement.textContent = text
+
+    // je souhaite que ma fonction me renvoit son résultat / réponde à ma question; je suis obligée de faire un return avec comme unique paramètre la réponse que j'attend (ici les nodeElement)
+    return(nodeElement)
+};
+
+
 // au clique sur le bouton
 button.addEventListener("click", () => {
     // creer une balise article
@@ -12,23 +25,19 @@ button.addEventListener("click", () => {
     // je l'insere dans la div root
     divRoot.appendChild(articleElement);
 
-    // avec un titre
-    const titleElement = document.createElement("h2");
-    titleElement.textContent = "belle image";
+    // je créer un élément titre
+    const titleElement = createElementNode("h2", "title-bis", "belle image");
     articleElement.appendChild(titleElement);
 
-    // une image
-    const imgElement = document.createElement("img");
-    imgElement.setAttribute(
-        "src",
-        "https://images.pexels.com/photos/17299063/pexels-photo-17299063/free-photo-of-bois-paysage-campagne-colline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        )
+    //je créer un élément image
+    const imgElement = createElementNode("img",
+     "", 
+     "", 
+     "https://images.pexels.com/photos/17299063/pexels-photo-17299063/free-photo-of-bois-paysage-campagne-colline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
     articleElement.appendChild(imgElement);
-    imgElement.style.hwidth = "300px";
+    imgElement.style.width = "300px";
 
-    // un paragraphe
-    const textElement = document.createElement("p");
-    textElement.textContent = "je suis belle image";
+    //je créer un élément paragraphe
+    const textElement = createElementNode("p", "", "je suis une belle image" );
     articleElement.appendChild(textElement);
-
 });
